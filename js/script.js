75b7423b211db75878daf21952c797121f8b7681
AetheRZX <aetherxz01@gmail.com>
2023-08-21 18:04:52 -0700

    Finished RPS website

--- js/script.js
+++ js/script.js
@@ -42,4 +42,137 @@ function isGameOver() {
 }
 
 //Interactive Element
+const scoreInfo = document.getElementById('scoreInfo');
+const scoreMessage = document.getElementById('scoreMessage');
 
+const playerScorePara = document.getElementById('player-score');
+const computerScorePara = document.getElementById('computer-score');
+const playerSign = document.getElementById('player-sign');
+const computerSign = document.getElementById('computer-sign');
+
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+
+const endgameModal = document.getElementById('endgameModal');
+const endgameMsg = document.getElementById('endgameMsg');
+const restartBtn = document.getElementById('restartBtn');
+const overlay = document.getElementById('overlay');
+
+rock.addEventListener("click", () => handleClick('ROCK'));
+paper.addEventListener("click", () => handleClick('PAPER'));
+scissors.addEventListener("click", () => handleClick('SCISSORS'));
+
+restartBtn.addEventListener('click', restartGame);
+overlay.addEventListener('click', closeEndgameModal);
+
+function handleClick(playerSelection)
+{
+  
+  if (isGameOver())
+  {
+    openEndgameModal();
+    return
+  }
+  
+  const computerSelection = getRandomChoice()
+  playRound(playerSelection, computerSelection)
+  updateChoices(playerSelection, computerSelection)
+  updateScore()
+
+  if (isGameOver())
+  {
+    openEndgameModal()
+    setFinalMessage()
+  }
+}
+
+function updateChoices(playerSelection, computerSelection)
+{
+  switch (playerSelection)
+  {
+    case 'ROCK':
+      playerSign.textContent = '✊'
+      break
+    case 'PAPER':
+      playerSign.textContent = '✋'
+      break
+    case 'SCISSORS':
+      playerSign.textContent = '✌'
+      break
+  }
+
+  switch (computerSelection)
+  {
+    case 'ROCK':
+      computerSign.textContent = '✊'
+      break
+    case 'PAPER':
+      computerSign.textContent = '✋'
+      break
+    case 'SCISSORS':
+      computerSign.textContent = '✌'
+      break
+  }
+}
+
+function updateScore() {
+  if (roundWinner === 'tie') {
+    scoreInfo.textContent = "It's a tie!"
+  } else if (roundWinner === 'player') {
+    scoreInfo.textContent = 'You won!'
+  } else if (roundWinner === 'computer') {
+    scoreInfo.textContent = 'You lost!'
+  }
+
+  playerScorePara.textContent = `Player: ${playerScore}`
+  computerScorePara.textContent = `Computer: ${computerScore}`
+}
+
+function updateScoreMessage(roundWinner, playerSelection, computerSelection)
+{
+  if (roundWinner == 'player')
+  {
+    scoreMessage.textContent = `${playerSelection} beats ${computerSelection.toLowerCase()}`
+  } 
+
+  if (roundWinner == 'computer')
+  {
+    scoreMessage.textContent = `${playerSelection} beaten by ${computerSelection.toLowerCase()}`
+  } 
+
+  if (roundWinner == 'tie')
+  {
+    scoreMessage.textContent = `${playerSelection} ties with ${computerSelection.toLowerCase()}`
+  }
+}
+
+function openEndgameModal() {
+  endgameModal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeEndgameModal() 
+{
+  endgameModal.classList.remove('active')
+  overlay.classList.remove('active')
+}
+
+function setFinalMessage() {
+  return playerScore > computerScore
+    ? (endgameMsg.textContent = 'You won!')
+    : (endgameMsg.textContent = 'You lost...')
+}
+
+function restartGame() {
+  playerScore = 0
+  computerScore = 0
+  scoreInfo.textContent = 'Choose your weapon'
+  scoreMessage.textContent = 'First to score 5 points wins the game'
+  playerScorePara.textContent = 'Player: 0'
+  computerScorePara.textContent = 'Computer: 0'
+  playerSign.textContent = '❔'
+  computerSign.textContent = '❔'
+  endgameModal.classList.remove('active')
+  overlay.classList.remove('active')
+}
